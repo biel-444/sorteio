63e77f13698f903d0b4ee4f62cedf3c088271b59
Gabriel-Davila-dc <gabrieldcdev@gmail.com>
2024-05-01 21:40:26 -0300

    foto times
    coloquei foto dos times e arrumei o backend

--- sorteador.js
+++ sorteador.js
@@ -1,13 +1,14 @@
 //Todos os times disponíveis
 var times = ['MANCHESTER CITY', 'REAL MADRID', 'ATALANTA', 'BORUSSIA DORTMUND','PARIS SAINT-GERMAIN','LIVERPOOL','BAYERN MÜNCHEN','ARSENAL','ATLÉTICO DE MADRID','INTERNAZIONALE','Porto','Bayer Leverkusen','Chelsea','Sporting',
+'Milan','RB Leipzig','Roma','Barcelona','Napoli','Real Sociedad','West Ham','Club Brugge','PSV Eindhoven','Benfica','Juventus','Lille','Aston Villa','Sevilla','Olympique Marseille','Freiburg','Manchester United','Internacional','Grêmio']
 
-'Riffa','Milan','RB Leipzig','Roma','Barcelona','Napoli','Real Sociedad','West Ham','Club Brugge','PSV Eindhoven','Benfica','Juventus','Lille','Aston Villa','Feyenoord','Sevilla','Sevilla','Olympique Marseille','Freiburg','Slavia Praga','FC Kobenhavn','Manchester United','Internacional','Grêmio']
+
 
 //variáveis
 var time1 = 'n'
 var time2 ='n'
 var min = 0
-var max = 37
+var max = 32
 min = Math.ceil(min)
 max = Math.floor(max)
 
