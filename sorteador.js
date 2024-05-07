4d3df1b71bccda4696288dfd6687a95ef62e278f
biel-444 <150447495+biel-444@users.noreply.github.com>
2024-05-06 22:47:44 -0300

    ee

--- sorteador.js
+++ sorteador.js
@@ -9,7 +9,7 @@ function cliqueDeSorteio(){
     //todas as URLs das fotos dos times
     var imgs = ["estilo/imagensTimes/manchester-city.png", "estilo/imagensTimes/logo-real-madrid-escudo-256.png", "estilo/imagensTimes/atalanta-bergamasca-calcio-256.png", "estilo/imagensTimes/borussia-dortmund-256.png", "estilo/imagensTimes/paris-saint-germain-256.png", "estilo/imagensTimes/liverpool-football-club-256.png", "estilo/imagensTimes/bayern-munchen-256.png", "estilo/imagensTimes/arsenal-football-club-256.png", "estilo/imagensTimes/logo-atletico-madrid-256.png", "estilo/imagensTimes/football-club-internazionale-milano-256.png", "estilo/imagensTimes/logo-porto-256.png", "estilo/imagensTimes/bayer-04-leverkusen-256.png", "estilo/imagensTimes/chelsea-football-club-256.png", "estilo/imagensTimes/logo-sporting-portugal-5.png", "estilo/imagensTimes/associazione-calcio-milan-256.png", "estilo/imagensTimes/rb-leipzig-256.png", "estilo/imagensTimes/logo-barcelona-256.png", "estilo/imagensTimes/societa-sportiva-calcio-napoli-256.png", "estilo/imagensTimes/logo-real-sociedad-256.png", "estilo/imagensTimes/west-ham-united-football-club-256.png", "estilo/imagensTimes/logo-brugge-5.png", "estilo/imagensTimes/logo-psv-eindhoven-5.png", "estilo/imagensTimes/logo-benfica-5.png", "estilo/imagensTimes/juventus-football-club-256.png", "estilo/imagensTimes/lille-olympique-sporting-club-256.png", "estilo/imagensTimes/aston-villa-football-club-256.png", "estilo/imagensTimes/logo-sevilla-256.png", "estilo/imagensTimes/olympique-de-marseille-256.png", "estilo/imagensTimes/sport-club-freiburg-256.png", "estilo/imagensTimes/manchester-united-football-club-256.png", "estilo/imagensTimes/logo-internacional-256.png", "estilo/imagensTimes/logo-gremio-256.png"]
 
-
+    
 
     //variáveis
     var time1 = 'n'
@@ -39,6 +39,7 @@ function cliqueDeSorteio(){
     //Retorna os nomes dos times para o HTML por id
     document.getElementById("vartime1").innerHTML = times[time1]
     document.getElementById("vartime2").innerHTML = times[time2]
+
 
     //manda as imagens dos times para o HTML por id
     var img11 = "estilo/imagensTimes/arsenal-football-club-256.png";
